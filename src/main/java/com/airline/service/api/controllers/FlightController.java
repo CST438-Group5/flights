b5074aca32a51a820b2daac349c14d2476c03ead
ThousandLeaves <chris.boyd87@ymail.com>
2021-06-06 21:33:13 -0700

    Started work on user registration / form validation

--- src/main/java/com/airline/service/api/controllers/FlightController.java
+++ src/main/java/com/airline/service/api/controllers/FlightController.java
@@ -1,14 +1,22 @@
 package com.airline.service.api.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.airline.service.api.domain.UserDto;
 import com.airline.service.api.entities.Flight;
 import com.airline.service.api.services.FlightService;
 
@@ -37,4 +45,26 @@ public class FlightController {
 	        model.addAttribute("flight", flight);
 	        return "index";
 	    }
+	   
+	   @GetMapping("/signup.html")
+	   public String showRegistrationForm(WebRequest request, Model model) {
+		   UserDto userDto = new UserDto();
+		   model.addAttribute("user", userDto);
+		   return "signup";
+	   }
+	   
+	   // TODO: ~In Progress~
+	   @PostMapping("/signup.html")
+	   public ModelAndView registerUserAccount(
+			   @ModelAtribute("user") @Valid UserDto userDto,
+			   HttpServletRequest request,
+			   Errors errors) {
+		   			
+		   		try {
+		   			User registered = userService.registerNewUserAccount(userDto);
+		   		} catch (UserAlreadyExistException uaeEx){
+		   			mav.addObject("message", "An account for that email address already exists!");
+		   			return mav;
+		   		}
+	   }
 }
